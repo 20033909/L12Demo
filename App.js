adbe0872e00201cd891b372f42b9171a238c23e6
20033909 <20033909@myrp.edu.sg>
2025-02-04 13:54:22 +0800

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {Gyroscope} from "expo-sensors";
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
 
 export default function App() {
+  const [{x, y, z}, setData] = useState({x:0, y:0, z:0});
+
+  useEffect(() => {
+      Gyroscope.setUpdateInterval(100);
+      const subscription= Gyroscope.addListener(setData);
+      return () => subscription.remove();
+
+
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar/>
+      <Text>x: {x}</Text>
+      <Text>y: {y}</Text>
+      <Text>z: {z}</Text>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
